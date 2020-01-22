cbfeb90800b641df7e3b5a533d2750f0db875594
Mauve3 <mauvemoonman@gmail.com>
2020-01-21 23:06:06 -0800

    fix: bug

--- app/components/CastPlayer/index.js
+++ app/components/CastPlayer/index.js
@@ -18,12 +18,7 @@ import playPress from './images/play-press.png';
 import audioOff from './images/audio_off.png';
 import audioOn from './images/audio_on.png';
 
-/* Local Player Class */
 import LocalPlayer from '../LocalPlayer';
-
-/* Chromecast Class */
-import CastWrapper from '../CastWrapper';
-
 import {
   setMediaPlayerObject,
   setMediaPlayerAvailable,
@@ -97,6 +92,364 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
+/** @enum {string} Constants of states for media for both local and remote playback */
+const PLAYER_STATE = {
+  // No media is loaded into the player. For remote playback, maps to
+  // the PlayerState.IDLE state.
+  IDLE: 'IDLE',
+  // Player is in PLAY mode but not actively playing content. For remote
+  // playback, maps to the PlayerState.BUFFERING state.
+  BUFFERING: 'BUFFERING',
+  // The media is loaded but not playing.
+  LOADED: 'LOADED',
+  // The media is playing. For remote playback, maps to the PlayerState.PLAYING state.
+  PLAYING: 'PLAYING',
+  // The media is paused. For remote playback, maps to the PlayerState.PAUSED state.
+  PAUSED: 'PAUSED',
+};
+
+const PlayerHandler = function() {
+  let currentTarget = {};
+
+  this.playerState = PLAYER_STATE.IDLE;
+  this.currentMedia = {};
+
+  this.setPlayerState = function(state) {
+    this.playerState = state;
+    console.info('State', state);
+  };
+
+  this.setTarget = function(target) {
+    currentTarget = target;
+  };
+
+  this.play = function() {
+    currentTarget.play();
+    this.setPlayerState(PLAYER_STATE.PLAYING);
+  };
+
+  this.pause = function() {
+    currentTarget.pause();
+    this.setPlayerState(PLAYER_STATE.PAUSED);
+  };
+
+  this.load = function(media) {
+    this.currentMedia = media;
+    if (currentTarget != null && 'load' in currentTarget) {
+      currentTarget.load(media);
+    }
+  };
+
+  this.getCurrentMediaTime = function() {
+    return currentTarget.getCurrentMediaTime();
+  };
+
+  this.getMediaDuration = function() {
+    return currentTarget.getMediaDuration();
+  };
+
+  this.seekTo = function(time) {
+    currentTarget.seekTo(time);
+  };
+
+  this.setVolume = function(pos) {
+    currentTarget.setVolume(pos);
+  };
+};
+
+const CastWrapper = function() {
+  this.playerHandler = new PlayerHandler(this);
+  this.playerHandler.lastPosRecorded = 0;
+};
+
+CastWrapper.prototype.initializeCastPlayer = function() {
+  const options = {};
+
+  // Set the receiver application ID to your own (created in the
+  // Google Cast Developer Console), or optionally
+  // use the chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID
+  options.receiverApplicationId = '72A3C8A4';
+
+  if (!chrome.cast) {
+    this.setPlayer(false);
+    return;
+  }
+
+  // Auto join policy can be one of the following three:
+  // ORIGIN_SCOPED - Auto connect from same appId and page origin
+  // TAB_AND_ORIGIN_SCOPED - Auto connect from same appId, page origin, and tab
+  // PAGE_SCOPED - No auto connect
+  options.autoJoinPolicy = chrome.cast.AutoJoinPolicy.TAB_AND_ORIGIN_SCOPED;
+
+  cast.framework.CastContext.getInstance().setOptions(options);
+
+  this.remotePlayer = new cast.framework.RemotePlayer();
+  this.remotePlayerController = new cast.framework.RemotePlayerController(
+    this.remotePlayer,
+  );
+  this.remotePlayerController.addEventListener(
+    cast.framework.RemotePlayerEventType.IS_CONNECTED_CHANGED,
+    function(e) {
+      this.setPlayer(e.value);
+    }.bind(this),
+  );
+  this.setupRemotePlayer();
+};
+
+CastWrapper.prototype.setPlayer = function(isCast) {
+  // If the player is remote
+  if (isCast) {
+    this.playerHandler.setIsLocalPlayer(false);
+    this.loadMedia(this.playerHandler.currentMedia);
+    if (this.playerHandler.localPlayer) this.playerHandler.localPlayer.pause();
+  } else {
+    // The player is local
+    this.playerHandler.setIsLocalPlayer(true);
+    // Start the local playback
+    if (this.playerHandler.localPlayer) {
+      if (this.playerHandler.localPlayer.getState().player.hasStarted) {
+        this.playerHandler.localPlayer.seek(this.playerHandler.lastPosRecorded);
+        this.playerHandler.localPlayer.play();
+      } else {
+        this.playerHandler.localPlayer.load();
+      }
+    }
+  }
+};
+
+CastWrapper.prototype.setupRemotePlayer = function() {
+  this.remotePlayerController.addEventListener(
+    cast.framework.RemotePlayerEventType.CURRENT_TIME_CHANGED,
+    function(event) {
+      //      console.log('CURRENT_TIME_CHANGED', event.value);
+      //      console.log('duration', this.remotePlayer.duration);
+      if (event.value !== 0) {
+        this.playerHandler.lastPosRecorded = event.value;
+        this.playerHandler.onSelectLoadingMedia(false);
+      }
+      if (this.remotePlayer.duration > 0) {
+        const seekPercent = parseInt(
+          (event.value / this.remotePlayer.duration) * 100,
+          10,
+        );
+        //        console.log(
+        //          'this.playerHandler.thisSeek != seekPercent',
+        //          this.playerHandler.thisSeek,
+        //          seekPercent,
+        //        );
+        if (this.playerHandler.thisSeek !== seekPercent) {
+          this.playerHandler.setSeek(seekPercent);
+        }
+      }
+    }.bind(this),
+  );
+
+  this.remotePlayerController.addEventListener(
+    cast.framework.RemotePlayerEventType.MEDIA_INFO_CHANGED,
+    function() {
+      const session = cast.framework.CastContext.getInstance().getCurrentSession();
+      if (!session) {
+        this.mediaInfo = null;
+        this.isLiveContent = false;
+        return;
+      }
+
+      const media = session.getMediaSession();
+      //      console.log('media', media);
+      if (!media) {
+        this.mediaInfo = null;
+        this.isLiveContent = false;
+      }
+    }.bind(this),
+  );
+
+  this.remotePlayerController.addEventListener(
+    cast.framework.RemotePlayerEventType.VIDEO_INFO_CHANGED,
+    function(event) {
+      console.info('VIDEO_INFO_CHANGED', event);
+    },
+  );
+
+  this.remotePlayerController.addEventListener(
+    cast.framework.RemotePlayerEventType.IS_MEDIA_LOADED_CHANGED,
+    function(event) {
+      console.info('IS_MEDIA_LOADED_CHANGED', event);
+      if (!event.value && !this.playerHandler.loadingMedia) {
+        console.info('Media Has Ended!');
+        this.playerHandler.lastPosRecorded = 0;
+        this.setNextChannelMedia();
+      } else {
+        console.info('Media Has Loaded!');
+      }
+    }.bind(this),
+  );
+
+  this.remotePlayerController.addEventListener(
+    cast.framework.RemotePlayerEventType.DURATION_CHANGED,
+    function(event) {
+      console.info('DURATION_CHANGED', event);
+    },
+  );
+
+  //  this.remotePlayerController.addEventListener(
+  //    cast.framework.RemotePlayerEventType.ANY_CHANGE,
+  //    function (event) {
+  //      console.log('ANY_CHANGE', event);
+  //    }.bind(this)
+  //  )
+
+  const playerTarget = {};
+
+  playerTarget.getCurrentMediaTime = function() {
+    return this.remotePlayer.currentTime;
+  }.bind(this);
+
+  playerTarget.getMediaDuration = function() {
+    return this.remotePlayer.duration;
+  }.bind(this);
+
+  playerTarget.seekTo = function(percent) {
+    const seek = (this.remotePlayer.duration / parseFloat(100)) * percent;
+    console.info('playerTarget.seekTo', seek);
+    this.remotePlayer.currentTime = seek;
+    this.remotePlayerController.seek();
+  }.bind(this);
+
+  playerTarget.setVolume = function(pos) {
+    console.info('playerTarget.setVolume', pos);
+    this.remotePlayer.volumeLevel = pos;
+    this.remotePlayerController.setVolumeLevel();
+  }.bind(this);
+
+  playerTarget.play = function() {
+    console.info('playerTarget.play');
+    if (this.remotePlayer.isPaused) {
+      this.remotePlayerController.playOrPause();
+    }
+  }.bind(this);
+
+  playerTarget.pause = function() {
+    console.info('playerTarget.pause');
+    if (!this.remotePlayer.isPaused) {
+      this.remotePlayerController.playOrPause();
+    }
+  }.bind(this);
+
+  playerTarget.load = function(meta) {
+    // The remote player isn't available. Load into the local player instead
+    if (cast.framework.CastContext.getInstance().getCurrentSession() === null) {
+      if ('load' in this.playerHandler.localPlayer) {
+        this.playerHandler.localPlayer.load();
+      } else {
+        console.log('error loading player', this.playerHandler)
+      }
+      return;
+    }
+
+    const mediaInfo = new chrome.cast.media.MediaInfo(meta.url, 'video/mp4');
+    mediaInfo.streamType = chrome.cast.media.StreamType.BUFFERED;
+    mediaInfo.metadata = new chrome.cast.media.TvShowMediaMetadata();
+    mediaInfo.metadata.title = meta.title;
+    mediaInfo.metadata.subtitle = meta.subtitle;
+    mediaInfo.metadata.images = [
+      {
+        url: meta.thumb,
+      },
+    ];
+
+    const request = new chrome.cast.media.LoadRequest(mediaInfo);
+    if (
+      this.playerHandler.localPlayer &&
+      this.playerHandler.localPlayer.getState().player.hasStarted
+    ) {
+      request.currentTime = this.playerHandler.localPlayer.getState().player.currentTime;
+      this.playerHandler.localPlayer.seek(0);
+    }
+    request.autoplay = true;
+
+    cast.framework.CastContext.getInstance()
+      .getCurrentSession()
+      .loadMedia(request)
+      .then(
+        function() {
+          console.info('Remote media loaded', request);
+        },
+        function(errorCode) {
+          console.error('Cast Error:', errorCode);
+          this.playerHandler.setPlayerState(PLAYER_STATE.IDLE);
+        },
+      );
+  }.bind(this);
+
+  this.playerHandler.setTarget(playerTarget);
+};
+
+CastWrapper.prototype.checkLoaded = () =>
+  cast.framework.CastContext.getInstance().getCurrentSession() !== null;
+
+CastWrapper.prototype.loadMedia = function(media) {
+  this.playerHandler.load(media);
+};
+
+CastWrapper.prototype.handleSeek = function(val) {
+  this.playerHandler.seekTo(val);
+};
+
+CastWrapper.prototype.handleVolume = function(val) {
+  this.playerHandler.setVolume(val / parseFloat(100));
+};
+
+CastWrapper.prototype.handlePause = function() {
+  this.playerHandler.pause();
+};
+
+CastWrapper.prototype.handlePlay = function() {
+  this.playerHandler.play();
+};
+
+CastWrapper.prototype.currentMedia = function() {
+  return this.playerHandler.currentMedia;
+};
+
+CastWrapper.prototype.setSeekBarRef = function(thisSeek, setSeek) {
+  this.playerHandler.thisSeek = thisSeek;
+  this.playerHandler.setSeek = setSeek;
+};
+
+CastWrapper.prototype.setIsLocalPlayerRef = function(
+  isLocalPlayer,
+  setIsLocalPlayer,
+) {
+  this.playerHandler.isLocalPlayer = isLocalPlayer;
+  this.playerHandler.setIsLocalPlayer = setIsLocalPlayer;
+};
+
+CastWrapper.prototype.setLocalPlayerRef = function(localPlayer) {
+  this.playerHandler.localPlayer = localPlayer;
+};
+
+CastWrapper.prototype.setMediaController = function(onSetMedia) {
+  this.playerHandler.onSetMedia = onSetMedia;
+};
+
+CastWrapper.prototype.setChannel = function(channel) {
+  this.playerHandler.channel = channel;
+};
+
+CastWrapper.prototype.setMediaLoadingFlags = function(
+  loadingMedia,
+  onSelectLoadingMedia,
+) {
+  this.playerHandler.loadingMedia = loadingMedia;
+  this.playerHandler.onSelectLoadingMedia = onSelectLoadingMedia;
+};
+
+CastWrapper.prototype.setNextChannelMedia = function() {
+  //  console.log("this.playerHandler.channel", this.playerHandler.channel)
+  //  var newMedia = this.playerHandler.channel.media[Math.floor(Math.random() * this.playerHandler.channel.media.length)];
+  this.playerHandler.onSetMedia();
+};
+
 function CastPlayer(props) {
   const {
     selected,
@@ -117,37 +470,74 @@ function CastPlayer(props) {
     url,
   };
 
+  console.log('channel', channel);
+  console.log('curmedia', curMedia);
+
   const [seek, setSeek] = useState(0);
   const [isLocalPlayer, setIsLocalPlayer] = useState(true);
   const [localPlayer, setLocalPlayerRef] = useState(React.createRef());
   let localLoading = false;
   let hasPlayedLocal = false;
+
   const classes = useStyles();
 
+
+  /*
+
+  TODO: Separate the code for the chromecast and local players
+
+  */
+
+  /* begin local code */
+
+  const handleLocalPlayerStateChange = function(state) {
+    if (
+      state.duration !== state.currentTime &&
+      state.currentTime > 0 &&
+      state.duration > 0
+    ) {
+      hasPlayedLocal = true;
+      onSelectLoadingMedia(false);
+    }
+    if (
+      !localLoading &&
+      playerAvailable &&
+      !player.checkLoaded() &&
+      state.ended &&
+      hasPlayedLocal &&
+      state.currentSrc === curMedia.url
+    ) {
+      localLoading = true;
+      player.setNextChannelMedia();
+    }
+  };
+
+  /* end local code */
+
   /* eslint-disable-next-line no-unused-vars */
-  const handleSeek = (e, val) => {
-    if (player) player.handleSeek(val);
+  let handleSeek = (e, val) => {
+    player.handleSeek(val);
   };
 
   /* eslint-disable-next-line no-unused-vars */
-  const handleVolume = (e, val) => {
-    if (player) player.handleVolume(val);
+  let handleVolume = (e, val) => {
+    player.handleVolume(val);
   };
 
   /* eslint-disable-next-line no-unused-vars */
-  const handlePause = (e, val) => {
-    if (player) player.handlePause();
+  let handlePause = (e, val) => {
+    player.handlePause();
   };
 
   /* eslint-disable-next-line no-unused-vars */
-  const handlePlay = (e, val) => {
-    if (player) player.handlePlay();
+  let handlePlay = (e, val) => {
+    player.handlePlay();
   };
 
   if (playerAvailable) {
     // For the chromecast to access the seekbar locally and movie it with media
     player.setSeekBarRef(seek, setSeek);
-    // A reference to the localplayer, so that chromecast can move it's seek and play when chromecast is stopped
+    // A reference to the localplayer, so that chromecast can move its seek and play when chromecast is stopped
     player.setLocalPlayerRef(localPlayer);
     // Shows chromecast when the local player is active and when it isn't
     player.setIsLocalPlayerRef(isLocalPlayer, setIsLocalPlayer);
@@ -162,48 +552,29 @@ function CastPlayer(props) {
       player.loadMedia(curMedia);
     }
 
-    /* Local player handler */
     if (localPlayer && 'subscribeToStateChange' in localPlayer) {
-      const handleLocalPlayerStateChange = function(state) {
-        if (
-          state.duration !== state.currentTime &&
-          state.currentTime > 0 &&
-          state.duration > 0
-        ) {
-          hasPlayedLocal = true;
-          onSelectLoadingMedia(false);
-        }
-        if (
-          !localLoading &&
-          playerAvailable &&
-          !player.checkLoaded() &&
-          state.ended &&
-          hasPlayedLocal &&
-          state.currentSrc === curMedia.url
-        ) {
-          localLoading = true;
-          player.setNextChannelMedia();
-        }
-      };
       localPlayer.subscribeToStateChange(handleLocalPlayerStateChange);
     }
   } else {
     const castWrapper = new CastWrapper();
-    // Run the init to setup the player locally first.
+    // A reference to the localplayer, so that chromecast can move its seek and play when chromecast is stopped
+    castWrapper.setLocalPlayerRef(localPlayer);
+    // Shows chromecast when the local player is active and when it isn't
     castWrapper.setIsLocalPlayerRef(isLocalPlayer, setIsLocalPlayer);
     castWrapper.initializeCastPlayer();
     onSelectPlayer(castWrapper);
-
-    // If the chromecast becomes available after load,
-    // run init again to resync
-    /* eslint-disable-next-line no-underscore-dangle */
-    window.__onGCastApiAvailable = function(isAvailable) {
+    window['__onGCastApiAvailable'] = function (isAvailable) {
       if (isAvailable) {
+        console.log('chromecast available!')
+        console.log('window', window)
+        // A reference to the localplayer, so that chromecast can move its seek and play when chromecast is stopped
+        castWrapper.setLocalPlayerRef(localPlayer);
+        // Shows chromecast when the local player is active and when it isn't
         castWrapper.setIsLocalPlayerRef(isLocalPlayer, setIsLocalPlayer);
         castWrapper.initializeCastPlayer();
         onSelectPlayer(castWrapper);
       } else {
-        console.error('error loading chromecast');
+        console.log('error loading chromecast')
       }
     };
   }
